fba42d5b86487e7c525cdf1f1efea4803e6a58c4
MagicJinn <jinn.bz@mail.com>
2025-03-02 20:23:12 +0100

    fixed horrendous indent formatting

--- JSON.cs
+++ JSON.cs
@@ -12,12 +12,12 @@ namespace SDLS
                 private static readonly JsonReader JSONReader = new JsonReader();
                 private static readonly JsonWriter JSONWriter = new JsonWriter();
 
-                public static string ReadGameJson(string fullFilePath)
-                {
-                        if (!File.Exists(fullFilePath)) return null; // Return null if the file is not found
+        public static string ReadFileSystemJson(string fullFilePath)
+        {
+            if (!File.Exists(fullFilePath)) return null; // Return null if the file is not found
 
-                        string str = File.ReadAllText(fullFilePath);
-                        return (str != null) ?
+            string str = File.ReadAllText(fullFilePath);
+            return (str != null) ?
                         (str.StartsWith("[") && str.EndsWith("]") ?
                          str.Substring(1, str.Length - 2) :
                          str) : // Return the string if the file isn't in an array
@@ -35,79 +35,101 @@ namespace SDLS
                         }
                         catch (Exception ex)
                         {
-                                Plugin.I.Error("Something went seriously wrong and SDLS was not able to load it's own embedded resource.");
-                                Plugin.I.Error(ex.Message);
-                                return "";
-                        }
-                }
+                Plugin.Instance.Error("Something went seriously wrong and SDLS was not able to load it's own embedded resource.");
+                Plugin.Instance.Error(ex.Message);
+                return "";
+            }
+        }
 
-                public static string Serialize(object obj)
+        public static string Serialize(object obj)
+        {
+            string serializedData = JSONWriter.Write(obj);
+            return serializedData;
+        }
+
+        public static Dictionary<string, object> Deserialize(string strObj)
+        {
+            var deserializedData = JSONReader.Read<Dictionary<string, object>>(strObj);
+            return deserializedData;
+        }
+
+        public static string[] SplitJSON(string strObjJoined)
+        {
+            var jsonObjects = new List<string>();
+            for (int i = 0, depth = 0, start = 0; i < strObjJoined.Length; i++)
+            {
+                if (strObjJoined[i] == '{' && depth++ == 0) start = i;
+                if (strObjJoined[i] == '}' && --depth == 0) jsonObjects.Add(strObjJoined.Substring(start, i - start + 1));
+            }
+            string[] jsonObjectsArray = jsonObjects.ToArray();
+            return jsonObjectsArray;
+        }
+
+        public static void CreateJSON(string strObjJoined, string relativeWritePath, bool hidden = false)
+        {
+            string writePath = Path.Combine(Plugin.PersistentDataPath, relativeWritePath) + ".json";
+            string path = Plugin.GetParentPath(writePath);
+
+            try
+            {
+                if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+                File.WriteAllText(writePath, relativeWritePath.ToLower().Contains("constants")
+                                    ? strObjJoined  // Output file as a single object
+                                    : $"[{strObjJoined}]"); // Put file in an array
+
+                if (hidden)
                 {
-                        string serializedData = JSONWriter.Write(obj);
-                        return serializedData;
+                    if (IsWindows())
+                    {
+                        File.SetAttributes(writePath, File.GetAttributes(writePath) | FileAttributes.Hidden);
+                    }
+                    else
+                    {
+                        // On Unix-based systems, hide the file by renaming it with a dot prefix
+                        string hiddenPath = Path.Combine(path, "." + Path.GetFileName(writePath));
+                        File.Move(writePath, hiddenPath);
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                Plugin.Instance.Error("Error writing file: " + ex.Message);
+            }
 
-                public static Dictionary<string, object> Deserialize(string strObj)
-                {
-                        var deserializedData = JSONReader.Read<Dictionary<string, object>>(strObj);
-                        return deserializedData;
-                }
 
-                public static string[] SplitJSON(string strObjJoined)
-                {
-                        var jsonObjects = new List<string>();
-                        for (int i = 0, depth = 0, start = 0; i < strObjJoined.Length; i++)
-                        {
-                                if (strObjJoined[i] == '{' && depth++ == 0) start = i;
-                                if (strObjJoined[i] == '}' && --depth == 0) jsonObjects.Add(strObjJoined.Substring(start, i - start + 1));
-                        }
-                        string[] jsonObjectsArray = jsonObjects.ToArray();
-                        return jsonObjectsArray;
-                }
+            bool IsWindows()
+            {
+                if (Environment.OSVersion.Platform.ToString().Contains("Win")) return true;
+                return false;
+            }
+        }
 
-                public static void CreateJSON(string strObjJoined, string relativeWritePath)
-                {
-                        string writePath = Path.Combine(Plugin.PersistentDataPath, relativeWritePath) + ".json";
-                        string path = Plugin.GetParentPath(writePath);
 
-                        try
-                        {
-                                if (!Directory.Exists(path)) Directory.CreateDirectory(path);
-                                File.WriteAllText(writePath, relativeWritePath.ToLower().Contains("constants")
-                                ? strObjJoined : // Output file as a single object
-                                $"[{strObjJoined}]"); // Put file in an array
 
-                        }
-                        catch (Exception ex)
-                        {
-                                Plugin.I.Error("Error writing file: " + ex.Message);
-                        }
-                }
+        public static void RemoveJSON(string fullpath)
+        {
+            string filePath = fullpath + ".json";
+            if (File.Exists(filePath))
+            {
+                Plugin.Instance.Warn("Removing " + fullpath);
+                File.Delete(filePath);
+            }
+        }
 
-                public static void RemoveJSON(string fullpath)
-                {
-                        string filePath = fullpath + ".json";
-                        if (File.Exists(filePath))
-                        {
-                                Plugin.I.Warn("Removing " + fullpath);
-                                File.Delete(filePath);
-                        }
-                }
+        public static string JoinJSON(List<string> strList, string sign = ",")
+        {
+            if (strList.Count == 0) return string.Empty;
 
-                public static string JoinJSON(List<string> strList, string sign = ",")
-                {
-                        if (strList.Count == 0) return string.Empty;
+            var sb = new StringBuilder();
+            for (int i = 0; i < strList.Count - 1; i++)
+            {
+                sb.Append(strList[i]).Append(sign);
+            }
+            sb.Append(strList[strList.Count - 1]);
+            return sb.ToString();
+        }
 
-                        var sb = new StringBuilder();
-                        for (int i = 0; i < strList.Count - 1; i++)
-                        {
-                                sb.Append(strList[i]).Append(sign);
-                        }
-                        sb.Append(strList[strList.Count - 1]);
-                        return sb.ToString();
-                }
-
-                public static string[] GetFilePaths() => new[]
+        public static string[] GetFilePaths() => new[]
                 {
                 "entities/qualities",
                 "entities/areas",
@@ -126,19 +148,5 @@ namespace SDLS
                 "constants/combatconstants",
                 "constants/navigationconstants"
                 };
-
-                // public static string[] ComponentsWithoutId() => new[]
-                // {
-                // "geography/TileRules",
-                // "geography/Tiles",
-                // "geography/TileSets",
-                // "encyclopaedia/CombatAttacks",
-                // "encyclopaedia/CombatItems",
-                // "encyclopaedia/SpawnedEntities",
-                // "encyclopaedia/Associations",
-                // "encyclopaedia/Flavours",
-                // "constants/combatconstants",
-                // "constants/navigationconstants"
-                // };
-        }
+    }
 }
